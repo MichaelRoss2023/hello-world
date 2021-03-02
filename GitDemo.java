bb294e9c90e96f07e497abf49deebd11927c17a1
MichaelRoss2023 <61289960+MichaelRoss2023@users.noreply.github.com>
2021-03-02 15:29:02 -0700

    Added name and reverseNum. Also fixed addNum

--- GitDemo.java
+++ GitDemo.java
@@ -1,10 +1,10 @@
 /*****
- * Edit this line to have your name.
+ Michael Ross
  ******/
 public class GitDemo {
     
     private double previousNumber;
-    
+    private double reverseNum;
     public GitDemo() {
         previousNumber = 1;
     }
@@ -13,7 +13,7 @@ public class GitDemo {
      * This function should add 'num' to 'previousNumber' and return the sum.
     *****/
     public double addNumber(double num) {
-        System.out.print("Add num to previousNumber");
+        System.out.print(num + previousNumber); //Adding numbers like requested
         return 0;
     }
     
@@ -45,7 +45,7 @@ public class GitDemo {
         for(int i = 1; i <= num; ++i){
             newNumber = newNumber * i;
         }
-        
+        reverseNum = newNumber; //unsure what its function is but it has been added
         return newNumber;
     }
     
